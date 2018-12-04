34915bbb4887258c0c5b6498f554088a89b828ba
chagacy <chay.agacy@hotmail.com>
2018-12-04 23:56:20 +0000

    Potion added and working
    Potions are added and the event doesn't refire anymore.

--- src/main/java/com/chayagacy/huemod/HueModEventHandler.java
+++ src/main/java/com/chayagacy/huemod/HueModEventHandler.java
@@ -84,10 +84,7 @@ public class HueModEventHandler {
 		EntityPlayer player = event.player;
 		if (player.getEntityWorld().getWorldTime() % 5 == 0) {
 			Iterable<BlockPos> blocks = BlockPos.getAllInBox(player.getPosition().add(-5, -5, -5),
-					player.getPosition().add(5, 5, 5)); // getEntityBoundingBox().expand(15,
-														// 15, 15));
-
-			// player.getHorizontalFacing()
+					player.getPosition().add(5, 5, 5)); // getEntityBoundingBox().expand(15, 15, 15));
 
 			for (BlockPos block : blocks) {
 				IBlockState state = player.getEntityWorld().getBlockState(block);
@@ -96,14 +93,7 @@ public class HueModEventHandler {
 					Vec3d blockPos = new Vec3d(block);
 					Vec3d diff = playerPos.subtract(blockPos);
 
-					float angle = (float) Math.toDegrees(Math.atan2(diff.z, diff.x)) + 180F; // Add
-																								// 180
-																								// so
-																								// it's
-																								// between
-																								// 0
-																								// and
-																								// 360
+					float angle = (float) Math.toDegrees(Math.atan2(diff.z, diff.x)) + 180F; // Add 180 so it's between 0 and 360
 					float rotation = ((player.getRotationYawHead() % 360) + 360) % 360; // First modulus gets -360 to 360, second gets 0 to 360
 					float relativeAngle = ((rotation - angle) + 180) % 360;
 
@@ -166,95 +156,20 @@ public class HueModEventHandler {
 
 	@SubscribeEvent
 	public static void onPotionTaken(PotionAddedEvent event) {
-		String potionName = event.getPotionEffect().getEffectName();
-		int colour = event.getPotionEffect().getPotion().getLiquidColor();
-		switch (potionName) {
-		case "effect.moveSpeed":
-			System.out.println("Swiftness " + colour);
-			System.out.println( Integer.toHexString(colour));
-			HueLightingEffects.flashBlue();
-			break;
-		case "effect.moveSlowdown":
-			System.out.println("effect.moveSlowdown");
-			break;
-		case "effect.digSpeed":
-			System.out.println("effect.digSpeed");
-			break;
-		case "effect.digSlowDown":
-			System.out.println("effect.digSlowDown");
-			break;
-		case "effect.damageBoost":
-			System.out.println("effect.damageBoost");
-			break;
-		case "effect.heal":
-			System.out.println("effect.heal");
-			break;
-		case "effect.harm":
-			System.out.println("effect.harm");
-			break;
-		case "effect.jump":
-			System.out.println("effect.jump");
-			break;
-		case "effect.confusion":
-			System.out.println("effect.confusion");
-			break;
-		case "effect.regeneration":
-			System.out.println("effect.regeneration");
-		case "effect.resistance":
-			System.out.println("effect.resistance");
-			break;
-		case "effect.fireResistance":
-			System.out.println("effect.fireResistance");
-			HueLightingEffects.flashRed();
-			break;
-		case "effect.waterBreathing":
-			System.out.println("effect.waterBreathing");
-			HueLightingEffects.flashGreen();
-			break;
-		case "effect.invisibility":
-			System.out.println("effect.invisibility");
-			break;
-		case "effect.blindness":
-			System.out.println("effect.blindness");
-			break;
-		case "effect.nightVision":
-			System.out.println("effect.nightVision");
-			break;
-		case "effect.hunger":
-			System.out.println("effect.hunger");
-			break;
-		case "effect.weakness":
-			System.out.println("effect.weakness");
-			break;
-		case "effect.poison":
-			System.out.println("effect.poison");
-			break;
-		case "effect.wither":
-			System.out.println("effect.wither");
-			break;
-		case "effect.healthBoost":
-			System.out.println("effect.healthBoost");
-			break;
-		case "effect.absorption":
-			System.out.println("effect.absorption");
-			break;
-		case "effect.saturation":
-			System.out.println("effect.saturation");
-			break;
-		case "effect.glowing":
-			System.out.println("effect.glowing");
-			break;
-		case "effect.levitation":
-			System.out.println("effect.levitation");
-			break;
-		case "effect.luck":
-			System.out.println("effect.luck");
-			break;
-		case "effect.unluck":
-			System.out.println("effect.unluck");
-			break;
-		}
-		// effect.moveSpeed
+		if (!event.getEntity().getEntityWorld().isRemote)
+			return;
+
+        PotionEffect pOld = event.getOldPotionEffect();
+        PotionEffect pNew = event.getPotionEffect();
+        if(pOld == null || pOld.getPotion() != pNew.getPotion() || pOld.getAmplifier() != pNew.getAmplifier())
+        {
+        	Potion potion =pNew.getPotion();
+    		int colourInt = potion.getLiquidColor();
+    		String colourHex = Integer.toHexString(colourInt);
+    		System.out.println(potion.getName() + " " + colourHex);
+    		
+    		// Reflect the colour of hte potion in the lights
+        }
 	}
 
 }

5029d5b2b33733422d6ba04ce42cc9f5cf39ef7f
chagacy <chay.agacy@hotmail.com>
2018-12-04 14:43:27 +0000

    Add more event catchers
    OnDie
    On block break
    underwater
    etc.

--- src/main/java/com/chayagacy/huemod/HueLightingEffects.java
+++ src/main/java/com/chayagacy/huemod/HueLightingEffects.java
@@ -63,4 +63,58 @@ public class HueLightingEffects {
 		effect.Enable();
 		Main.getHueStream().UnlockMixer();
 	}
+	
+	public static void flashGreen() {
+
+		// Create an animation which is fixed 0
+		ConstantAnimation fixedZero = new ConstantAnimation(0.0);
+
+		// Create an animation which repeats a 1 second sawTooth 5 times
+		PointVector pointList = new PointVector();
+		pointList.add(new Point(0, 0));
+		pointList.add(new Point(500, 1.0));
+		pointList.add(new Point(1000, 0.0));
+		double repeatTimes = 0;
+		CurveAnimation sawTooth = new CurveAnimation(repeatTimes, pointList);
+
+		// Create an effect on the left half of the room where blue is animated
+		// by sawTooth
+		AreaEffect effect = new AreaEffect("", 0);
+		effect.AddArea(Area.getAll());
+		effect.SetColorAnimation(fixedZero, sawTooth, fixedZero); // r=0, g=0,
+																	// b=sawTooth
+
+		// Now play both effects
+		Main.getHueStream().LockMixer();
+		Main.getHueStream().AddEffect(effect);
+		effect.Enable();
+		Main.getHueStream().UnlockMixer();
+	}
+	
+	public static void flashBlue() {
+
+		// Create an animation which is fixed 0
+		ConstantAnimation fixedZero = new ConstantAnimation(0.0);
+
+		// Create an animation which repeats a 1 second sawTooth 5 times
+		PointVector pointList = new PointVector();
+		pointList.add(new Point(0, 0));
+		pointList.add(new Point(500, 1.0));
+		pointList.add(new Point(1000, 0.0));
+		double repeatTimes = 0;
+		CurveAnimation sawTooth = new CurveAnimation(repeatTimes, pointList);
+
+		// Create an effect on the left half of the room where blue is animated
+		// by sawTooth
+		AreaEffect effect = new AreaEffect("", 0);
+		effect.AddArea(Area.getAll());
+		effect.SetColorAnimation(fixedZero, fixedZero, sawTooth); // r=0, g=0,
+																	// b=sawTooth
+
+		// Now play both effects
+		Main.getHueStream().LockMixer();
+		Main.getHueStream().AddEffect(effect);
+		effect.Enable();
+		Main.getHueStream().UnlockMixer();
+	}
 }
--- src/main/java/com/chayagacy/huemod/HueModEventHandler.java
+++ src/main/java/com/chayagacy/huemod/HueModEventHandler.java
@@ -14,25 +14,34 @@ import com.lighting.huestream.PointVector;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFire;
+import net.minecraft.block.BlockOre;
+import net.minecraft.block.BlockTorch;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.terraingen.BiomeEvent;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
@@ -40,7 +49,24 @@ import net.minecraftforge.fml.relauncher.Side;
 public class HueModEventHandler {
 
 	@SubscribeEvent
-	public static void onHit(LivingHurtEvent event) {
+	public static void onHurt(LivingHurtEvent event) {
+		if (event.getEntity() instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer)event.getEntity();
+			if(player.isPotionActive(MobEffects.POISON)){
+				HueLightingEffects.flashGreen();
+			} 
+			else if(player.isPotionActive(MobEffects.WITHER)){
+				//Purple
+				
+			}
+			else //if (event.getSource().getImmediateSource() instanceof EntityPlayer) {
+				HueLightingEffects.flashRed();
+			//}
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onDie(LivingDeathEvent event) {
 		if (event.getEntity() instanceof EntityPlayer) {
 			HueLightingEffects.flashRed();
 		}
@@ -50,31 +76,11 @@ public class HueModEventHandler {
 	public static void onExplosion(ExplosionEvent event) {
 		HueLightingEffects.flashRed();
 	}
-
-
-    @SubscribeEvent
-    public static void onPlayerTick(TickEvent.PlayerTickEvent event)
-    {
-        if(event.phase != TickEvent.Phase.END || event.side != Side.CLIENT)
-            return;
-        EntityPlayer player = event.player;
-        Vec3d playerPos = player.getPositionVector();
-        Vec3d blockPos = new Vec3d(-28.5, 67.5, 47.5);
-        Vec3d diff = playerPos.subtract(blockPos);
-
-        float angle = (float) Math.toDegrees(Math.atan2(diff.z, diff.x)) + 180F; //Add 180 so it's between 0 and 360
-        float rotation = ((player.getRotationYawHead() % 360) + 360) % 360; //First modulus gets -360 to 360, second gets 0 to 360
-
-        //RunicMagic.LOG.info("Angle -> Degrees: {}, Rotation: {}", angle, rotation);
-
-        float relativeAngle = ((rotation - angle) + 180) % 360;
-
-        //RunicMagic.LOG.info("Relative Angle -> {}", relativeAngle);
-    }
-	
 	
 	@SubscribeEvent
-	public static void onTicketEvent(PlayerTickEvent event) {
+	public static void onTicketEvent(TickEvent.PlayerTickEvent event) {
+        if(event.phase != TickEvent.Phase.END || event.side != Side.CLIENT)
+            return;
 		EntityPlayer player = event.player;
 		if(player.getEntityWorld().getTotalWorldTime() % 5 == 0){
 			Iterable<BlockPos> blocks = BlockPos.getAllInBox(player.getPosition().add(-5,-5,-5), player.getPosition().add(5,5,5)); //getEntityBoundingBox().expand(15, 15, 15));
@@ -92,10 +98,21 @@ public class HueModEventHandler {
 			        float rotation = ((player.getRotationYawHead() % 360) + 360) % 360; //First modulus gets -360 to 360, second gets 0 to 360
 			        float relativeAngle = ((rotation - angle) + 180) % 360;
 			        
-			        
-//					int fireAngle = (int) Math.toDegrees(Math.atan2(player.posZ -block.getZ() ,player.posX -  block.getX())); // between 180 and -180
-//					int playerAngle = (int) Math.abs(player.rotationYawHead %360);
 					System.out.println(relativeAngle);
+					
+					HueLightingEffects.flashRed();
+				}
+				else if (state.getBlock() instanceof BlockTorch){ // Add a bright constant light. Should it be direction?????
+					Vec3d playerPos = player.getPositionVector();
+			        Vec3d blockPos = new Vec3d(block);
+					Vec3d diff = playerPos.subtract(blockPos);
+					
+			        float angle = (float) Math.toDegrees(Math.atan2(diff.z, diff.x)) + 180F; //Add 180 so it's between 0 and 360
+			        float rotation = ((player.getRotationYawHead() % 360) + 360) % 360; //First modulus gets -360 to 360, second gets 0 to 360
+			        float relativeAngle = ((rotation - angle) + 180) % 360;
+			        
+					System.out.println(relativeAngle);
+					
 					
 					HueLightingEffects.flashRed();
 				}
@@ -103,13 +120,49 @@ public class HueModEventHandler {
 			
 			
 			//////////////////UNDERWATER
-			
-			Biome biome = player.getEntityWorld().getBiomeForCoordsBody(player.getPosition());
-			if(biome.equals("WATER")){
+			if (player.getAir()<300){
 				System.out.println("WATER");
+				HueLightingEffects.flashBlue();
 			}
 			
 		}
 	}
+	
+	@SubscribeEvent
+	public static void onSound(PlaySoundEvent event) {
+		if (event.getName().equals("block.portal.trigger")){ // Nether portal
+			System.out.println("PORTAL");
+		} 
+		else if(event.getName().equals("entity.lightning.impact")){ // lightning
+			HueLightingEffects.flashBlue();
+		} 
+	}
+	
+	@SubscribeEvent
+	public static void onBlockBreak(BreakEvent event) {
+		if (event.getState().getBlock() instanceof BlockOre){
+			if(event.getState().getBlock() == Blocks.GOLD_ORE){
+				System.out.println("GOLD");
+			} else if(event.getState().getBlock() == Blocks.IRON_ORE){
+				System.out.println("IRON");
+			} else if(event.getState().getBlock() == Blocks.DIAMOND_ORE){
+				System.out.println("DIAMOND");
+			} else if(event.getState().getBlock() == Blocks.COAL_ORE){
+				System.out.println("COAL");
+			} else if(event.getState().getBlock() == Blocks.REDSTONE_ORE){
+				System.out.println("REDSTONE");
+			} else if(event.getState().getBlock() == Blocks.EMERALD_ORE){
+				System.out.println("EMERALD");
+			} else if(event.getState().getBlock() == Blocks.LAPIS_ORE){
+				System.out.println("LAPIS");
+			}
+		} 
+	}
+	
+//	@SubscribeEvent
+//	public static void onPotionTaken(PotionAddedEvent event) {
+//
+//	}	
+	
 
 }
